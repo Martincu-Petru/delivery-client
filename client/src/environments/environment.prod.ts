9a281d5c8ccf2b94478edb7eae146c5c59221950
Marțincu Petru <martincu.petru@gmail.com>
2019-05-31 06:12:14 +0300

    Built for production

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
-  production: true
+  production: true,
+  getUsernameURL: 'http://localhost:5000/user/session?',
+  getCourierUsernameURL: 'http://localhost:5000/courier/session?',
+  getRestaurantsURL: 'http://localhost:5000/restaurants',
+  getRestaurantDetailsURL: 'https://tema3cloudcomputing.appspot.com/restaurant?',
+  getUserURL: 'http://localhost:5000/user?',
+  getCourierURL: 'http://localhost:5000/courier?',
+  postUserURL: 'http://localhost:5000/user',
+  postCourierURL: 'http://localhost:5000/courier',
+  getSessionURL: 'http://localhost:5000/session?',
+  getSessionCourierURL: 'http://localhost:5000/get/courier/session?',
+  postSessionURL: 'http://localhost:5000/session',
+  postSessionCourierURL: 'http://localhost:5000/courier/session',
+  getItemsUrl: 'http://localhost:5000/items?',
+  postOrdersURL: 'http://localhost:5000/orders',
+  getOrderURL: 'http://localhost:5000/orders?',
+  completeOrderURL: 'http://localhost:5000/complete?'
 };
